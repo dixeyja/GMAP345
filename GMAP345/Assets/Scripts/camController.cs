b7f3487603f802084c55eee470633ef20be4a25a
dixeyja <39249054+dixeyja@users.noreply.github.com>
2019-07-08 00:36:42 -0400

    Basic Character Controller + Imported Unity Standard Assets

---
+++ GMAP345/Assets/Scripts/camController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camController : MonoBehaviour
+{
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    [SerializeField]
+    private float sensitivity = 5.0f;
+    [SerializeField]
+    private float smoothing = 2.0f;
+
+    private GameObject playerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCharacter = transform.parent.gameObject;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        playerCharacter.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, playerCharacter.transform.up);
+    }
+}
---
+++ GMAP345/Assets/Scripts/characterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translate = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translate *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translate);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public float Speed
+    {
+        get; set;
+    }
+}
